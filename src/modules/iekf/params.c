9031c0dba2ef6f751fac36e455732b89cd236a70
James Goppert <james.goppert@gmail.com>
2017-01-20 13:02:40 -0500

    Adds IEKF estimator and ROS->uORB translator

---
+++ src/modules/iekf/params.c
@@ -0,0 +1,14 @@
+#include <systemlib/param/param.h>
+
+// 16 is max name length
+
+/**
+ * Test 1
+ *
+ * @group IEKF
+ * @unit m
+ * @min -1
+ * @max 1
+ * @decimal 3
+ */
+PARAM_DEFINE_FLOAT(IEKF_TEST1, 1.0f);
